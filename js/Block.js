afec9af1b733ff2e63c72382aeffbad8c334b0fa
ShreyasGhate <74915640+ShreyasGhate@users.noreply.github.com>
2021-04-06 19:41:05 +0530

    Add files via upload

---
+++ js/Block.js
@@ -0,0 +1,39 @@
+class Block{
+    constructor(x, y, width, height) {
+        var options = {
+           
+            restitution :0.4,
+            friction :0.0,
+            //isStatic:true
+           
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = 50;
+        this.height = 50;
+        this.scale = 1;
+        this.image = loadImage("bottle.png");
+        World.add(world, this.body);
+      }
+      display(){
+        
+        if(this.body.speed < 5){
+          var angle = this.body.angle;
+        var pos= this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image,0,0,this.width, this.height);
+        pop();
+         }
+         else{
+           World.remove(world, this.body);
+           push();
+           this.Visiblity = this.Visiblity - 5;
+           tint(255,this.Visiblity);
+           
+           pop();
+         }
+        
+      }
+}
